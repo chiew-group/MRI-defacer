d5ee31ad4a7a2daf98983d81ef9b3b41ddf51173
selinlyx <selinaliu228@gmail.com>
2025-06-12 10:52:10 -0400

    adding comments

--- main.py
+++ main.py
@@ -6,60 +6,59 @@ from ROVir import top_nv_sir
 from ROVir import form_virtual_coil_data
 import time
 
-#loading npy MRI data, shape (240, 240, 48, 252) = (x, y, channels, z) note: not always the case for shape
+# loading npy MRI data, shape (120, 120, 48, 126) = (x, y, channels, z) note: not always the case for shape
 data = np.load(r"C:\Users\selin\OneDrive\Desktop\SRI\MRI defacer\reduced_data.npy") #loading MRI data, already centered
 
-#downsize data by  2x in every aces 
+# defining image slices
+x_slice = 60 # input("Enter x slice for sagittal view")
+y_slice = 60 # input("Enter y slice for coronal view")
+z_slice = 60 # input("Enter z slice for axial view"))
 
-x_slice = 60 # why are both ends visible, not central slices? need to shift before rss and slicing
-y_slice = 60 
-z_slice = 60 
+maskA = np.zeros((data.shape[0], data.shape[1], data.shape[3])) #define signal mask
+maskB = np.ones((data.shape[0], data.shape[1], data.shape[3])) # define inteference mask
 
-# defining manual masks for ROVir algorithm
-# mask A is the region of interest, mask B is the region of interference
+# margin = 50
+# maskA[margin:-margin+20, margin:-margin+20, margin:-margin+20] = 1
+# maskB[margin:-margin+20, margin:-margin+20, margin:-margin+20] = 0
+# maskA[20:70, 20:70, :] = 1
+# maskB[25:65, 25:65, :]  = 0
+# maskA[:, 30:60, :] = 1
+# maskB[:, 30:55, : ] = 0
 
-maskA = np.zeros((data.shape[0], data.shape[1], data.shape[3]))
-maskB = np.ones((data.shape[0], data.shape[1], data.shape[3])) # a 3D matrix of zeros
-margin = 20
-maskA[margin+30:-margin, margin+30:-margin, margin+30:-margin] = 1
-maskB[margin+30:-margin, margin+30:-margin, margin+30:-margin] = 0
+V = rovir(data, maskA, maskB) #finding the eigenvectors for Av = λBv
 
-start = time.time()
-V = rovir(data, maskA, maskB) #finding the eigenvectors 
-end = time.time()
-print(f"Time taken for ROVir: {end - start} seconds")
 
-start = time.time()
-# now choose the top nv eigencvectors
-# calculate signal interference ratio for each channel, keep only those with sir > 1
-# sir_threshold = input("Enter SIR thresshold, default is 1")
-i = top_nv_sir(V, data, maskA, maskB, sir_threshold = 4) #finding 
-print(i)
-V_retain = V[:,:i] # keep only the top nv eigenvectors 
+i = top_nv_sir(V, data, maskA, maskB, sir_threshold = 3) # finding top nv eigenvectors based on SIR
+print(f'The top nv eigenvectors contain the first {i} eigenvectors')
+V_retain = V[:,:] # retain only the top nv eigenvectors 
 
-end = time.time()
-print(f"Time taken for top_nv: {end - start} seconds")
+virtual_coil_data = form_virtual_coil_data(V_retain, data) # forming virtual coils, with eigenvectors as linear combo weights
 
-start = time.time()
-# now, the eigenvectors make up the linear combo weights
-# so we need to form the virtual coil data
-virtual_coil_data = form_virtual_coil_data(V_retain, data)
-end = time.time()
-print(f"Time taken for form_virtual_coil_data: {end - start} seconds")
-
-# fxn to compute the root sum of squares to combine channels
 def rsos(data):
+    '''
+    Computes the root sum of squares of the data across the channel dimension.
+    
+    Parameters
+    ----------
+        data -> np.ndarray: a 4D array of MRI data, shape (x, y, ch, z)
+
+    Returns
+    ----------
+        np.ndarray: a 3D array of the root sum of squares of the data, shape (x, y, z)
+    '''
+    
     return np.sqrt(np.sum(np.abs(data)**2, axis=2))
 
-image = np.fft.fftshift(np.fft.ifftn(np.fft.fftshift(virtual_coil_data, axes = (0, 1, 3)), axes=(0, 1, 3)), axes = (0, 1, 3)) # compute the Fourier transform of data over x, y, z axes
-image_rss = rsos(image) 
+# shift, compute fourier transform, shift over x, y, z axes
+image = np.fft.fftshift(np.fft.ifftn(np.fft.fftshift(virtual_coil_data, axes = (0, 1, 3)), axes=(0, 1, 3)), axes = (0, 1, 3))
+image_rss = rsos(image) # find root sum of squares of image over the channel dimension
 
 figure = plt.figure(figsize = (20, 20))
 for coil in range(virtual_coil_data.shape[2]):
     plt.subplot(8, 6, coil+1)
-    plt.imshow(np.abs(image[:, :, coil, z_slice]), cmap='gray')
-    plt.imshow(maskA[:, :, z_slice], alpha = 0.2, cmap='Greens')
-    plt.imshow(maskB[:, :, z_slice], alpha = 0.2, cmap='Reds')
+    plt.imshow(np.abs(np.rot90(image[x_slice, :, coil, :],3)), cmap='gray')
+    # plt.imshow(maskA[x_slice, :, :], alpha = 0.2, cmap='Greens')
+    # plt.imshow(maskB[x_slice, :, :], alpha = 0.2, cmap='Reds')
     plt.title(f'Virtual Coil {coil+1}')
 plt.show()
 
@@ -91,8 +90,6 @@ plt.imshow(np.log(np.abs(kspace_rss_axial)+1e-9), cmap='gray')
 plt.subplot(3,2,2)
 plt.title(f'Axial View z = {z_slice}')
 plt.imshow(np.rot90(image_axial,3), cmap='gray')
-plt.imshow(maskA[:, :, z_slice], alpha = 0.2, cmap='Greens')
-plt.imshow(maskB[:, :, z_slice], alpha = 0.2, cmap='Reds')
 
 plt.subplot(3,2,3)
 plt.title(f'Coronal K-space y = {y_slice}')
@@ -109,11 +106,10 @@ plt.imshow(np.log(np.abs(kspace_rss_sag)+1e-9), cmap='gray')
 plt.subplot(3,2,6)
 plt.title(f'Sagittal View x = {x_slice}')
 plt.imshow(np.rot90(image_sag,3), cmap='gray')
+# plt.imshow(maskA[x_slice, :, :], alpha = 0.2, cmap='Greens')
+# plt.imshow(maskB[x_slice, :, :], alpha = 0.2, cmap='Reds')
 
 name = f'Reconstructed Image Axial {z_slice}, Coronal {y_slice}, Sagittal {x_slice}.png'
 path = os.path.join(r"C:\Users\selin\OneDrive\Desktop\SRI\SRI Code", name)
 plt.savefig(path)
 plt.show() 
-
-
-#assume mrd format for input data 
