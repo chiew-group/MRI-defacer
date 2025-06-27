6452b9d1e2db39de424bda3c1d6e970b50bf661e
selinlyx <selinaliu228@gmail.com>
2025-06-27 14:47:09 -0400

    get rid of file explorer

--- file_explorer.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import filedialog
-
-def select_data():
-    window = Tk()
-    window.withdraw()
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("All files", "*.*"),))
-    return filename
-                                                                                                 
