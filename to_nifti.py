3f07bbc733fe0150fe356202d57ea1ca15368397
selinlyx <selinaliu228@gmail.com>
2025-08-13 11:35:55 -0400

    update nifti saver

--- to_nifti.py
+++ to_nifti.py
@@ -4,10 +4,9 @@ import nibabel as nib
 GREEN = '\033[92m'
 RESET = '\033[0m'
 
-def niftify(image, a11, a22, a33):
+def niftify(image, a11, a22, a33, name):
     
     image = (image - image.min()) / (image.max() - image.min()) * 255
-
 
     # converting numpy data to nifti file 
     affine = np.array([[a11, 0.0, 0.0, -image.shape[0]//2], 
@@ -19,5 +18,6 @@ def niftify(image, a11, a22, a33):
     nifti_img = nib.Nifti1Image(image.astype(np.uint8), affine)
     print(nifti_img.affine)
 
-    nib.save(nifti_img, r"results\input_image.nii")
+    fp = f'results/{name}.nii'
+    nib.save(nifti_img, fp)
     print(GREEN + "Nifti file successfully saved" + RESET)
