51bfd5220e59ab2bc5d30b77467fbcf546e0ba1b
selinlyx <selinaliu228@gmail.com>
2026-06-16 02:17:27 -0400

    implement ANTsPy for segmentation

--- automask.py
+++ automask.py
@@ -3,15 +3,13 @@ import nibabel as nib
 import os
 import subprocess
 import shutil
-# from scipy.ndimage import zoom
+import ants
+import antspynet
 
 GREEN = '\033[92m'
 RESET = '\033[0m'
 
-'''
-Wrapper for CHARM CLI tool.
 
-'''
 def gen_mask(input_fp, output_fp, saveID):
         '''
         To generate numpy binary masks for the brain and face regions using GRACE CLI. 
@@ -22,50 +20,25 @@ def gen_mask(input_fp, output_fp, saveID):
         output_fq -> str: path to output nifti masks of face and brain
         saveID -> str: ID to save numpy masks 
         '''
-        charm_path = shutil.which("charm.cmd") # user must have SimNIBS installed to path
-        command = [charm_path,
-                   saveID,
-                   input_fp, "--noneck", "--forcesform", "--forcerun"]
-        
-        subprocess.run(command) # run CHARM CLI tool
-        
-        seg_img = nib.load(f'm2m_{saveID}/final_tissues.nii.gz') # load segmentation results 
-        all_tissues = seg_img.get_fdata()
-        
-        lut_pf = f"m2m_{saveID}/final_tissues_LUT.txt" # file path for look up table
 
-        name_key_map = {} # to store mapping for label name and key in segmentation file
+        input = ants.image_read(input_fp) # load input data
 
-        # use the look up table to find the segmentation labels 
-        with open(lut_pf, 'r') as LUT:
-                for line in LUT:
-                        if not line.strip() or line.startswith('#'):
-                                continue # skip empty lines and first line
-                        
-                        line_split = line.split() 
+        whole_head = ants.get_mask(input, cleanup = 1) # get mask of entire head
+        # ants.image_write(whole_head, 'segmentations/ant_whole_head.nii.gz') # save whole head data
 
-                        # add the tissue name and tissues code
-                        name_key_map[line_split[1]] = int(line_split[0])
+        brain_probability_mask = antspynet.utilities.brain_extraction(image=input, modality="t1")
+        brain_bin_mask = ants.threshold_image(brain_probability_mask, low_thresh=0.5, high_thresh=1.0, binary=True)
+        ants.image_write(brain_bin_mask, os.path.join(output_fp, 'brain.nii.gz'))
 
-        brain_names = ['White-Matter', 'Gray-Matter', 'CSF']
-        brain_labels = [name_key_map[name] for name in brain_names]
-        
-        
-        face_names = ['Bone', 'Scalp', 
-                      'Eye_balls', 'Muscle', 
-                      'Cartilage', 'Fat']
-        face_labels = [name_key_map[name] for name in face_names]
-        
-        brain_mask = np.isin(all_tissues, brain_labels).astype(np.uint8)
-        face_mask = np.isin(all_tissues, face_labels).astype(np.uint8)
+        whole_head_np = whole_head.numpy().astype(bool)
+        brain_bin_mask_np = brain_bin_mask.numpy().astype(bool)
 
-        os.makedirs(output_fp, exist_ok=True)
+        face_np = whole_head_np & ~brain_bin_mask_np # logical AND NOT
 
-        brain_fp = os.path.join(output_fp, "brain.nii.gz")
-        face_fp = os.path.join(output_fp, "face.nii.gz")
-        nib.save(nib.Nifti1Image(brain_mask, seg_img.affine), brain_fp)
-        nib.save(nib.Nifti1Image(face_mask, seg_img.affine), face_fp)
-
-        print(GREEN + "Mask saved successfully" + RESET)
+        face = ants.from_numpy(face_np.astype(np.uint8), 
+                        origin=whole_head.origin, 
+                        spacing=whole_head.spacing, 
+                        direction=whole_head.direction)
+        ants.image_write(face, os.path.join(output_fp, 'face.nii.gz'))
 
         print(GREEN + "Mask saved successfully" + RESET)
