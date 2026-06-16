3ae1bbe692b8820577dcfed955f8475faae80043
selinlyx <selinaliu228@gmail.com>
2026-06-16 14:49:23 -0400

    update comments

--- automask.py
+++ automask.py
@@ -12,7 +12,7 @@ RESET = '\033[0m'
 
 def gen_mask(input_fp, output_fp, saveID):
         '''
-        To generate numpy binary masks for the brain and face regions using GRACE CLI. 
+        To generate numpy binary masks for the brain and face regions using ANTsPY and ANTsPyNet.
         
         Parameters
         ----------
@@ -24,21 +24,22 @@ def gen_mask(input_fp, output_fp, saveID):
         input = ants.image_read(input_fp) # load input data
 
         whole_head = ants.get_mask(input, cleanup = 1) # get mask of entire head
-        # ants.image_write(whole_head, 'segmentations/ant_whole_head.nii.gz') # save whole head data
 
-        brain_probability_mask = antspynet.utilities.brain_extraction(image=input, modality="t1")
-        brain_bin_mask = ants.threshold_image(brain_probability_mask, low_thresh=0.5, high_thresh=1.0, binary=True)
-        ants.image_write(brain_bin_mask, os.path.join(output_fp, 'brain.nii.gz'))
+        os.makedirs(output_fp, exist_ok=True) # make an output directory to store outputs
 
-        whole_head_np = whole_head.numpy().astype(bool)
+        brain_probability_mask = antspynet.utilities.brain_extraction(image=input, modality="t1") # get probability mask for brain
+        brain_bin_mask = ants.threshold_image(brain_probability_mask, low_thresh=0.5, high_thresh=1.0, binary=True) # get binary mask for brain
+        ants.image_write(brain_bin_mask, os.path.join(output_fp, 'brain.nii.gz')) # save nifti of binary brain mask
+
+        whole_head_np = whole_head.numpy().astype(bool) 
         brain_bin_mask_np = brain_bin_mask.numpy().astype(bool)
 
-        face_np = whole_head_np & ~brain_bin_mask_np # logical AND NOT
+        face_np = whole_head_np & ~brain_bin_mask_np # logical AND NOT to find the face mask
 
         face = ants.from_numpy(face_np.astype(np.uint8), 
                         origin=whole_head.origin, 
                         spacing=whole_head.spacing, 
                         direction=whole_head.direction)
-        ants.image_write(face, os.path.join(output_fp, 'face.nii.gz'))
-
+        ants.image_write(face, os.path.join(output_fp, 'face.nii.gz')) # save nifti of binary face mask
+ 
         print(GREEN + "Mask saved successfully" + RESET)
